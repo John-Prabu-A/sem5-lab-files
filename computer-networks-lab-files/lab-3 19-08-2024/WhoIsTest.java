7d79f94bb0342afa56b3e283e86bed0380115b9f
John Prabu A <johnprabu0702@gmail.com>
2024-08-30 18:10:43 +0530

    Basic Socket Programming InetAddress and WhoIs learning

--- computer-networks-lab-files/lab-3 19-08-2024/WhoIsTest.java
+++ computer-networks-lab-files/lab-3 19-08-2024/WhoIsTest.java
@@ -1,15 +1,21 @@
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class WhoIsTest {
-    public static void main(String args[]) throws UnknownHostException {
+    public static void main(String args[]) throws UnknownHostException, IOException {
         int c;
         Socket s = new Socket("whois.internic.net", 43);
         InputStream in = s.getInputStream();
         OutputStream out = s.getOutputStream();
-        String str = 
-
+        String str = (args.length == 0 || args[0].isEmpty() ? "google.com" : args[0]) + "\n";
+        byte buff[] = str.getBytes("UTF-8");
+        out.write(buff);
+        while((c = in.read()) != -1) {
+            System.out.print((char) c);
+        }
+        s.close();
     }
 }
