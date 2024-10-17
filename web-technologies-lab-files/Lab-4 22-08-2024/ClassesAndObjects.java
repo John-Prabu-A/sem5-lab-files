80ff497d7e05f659645780ef89db3d9285e5b147
John Prabu A <johnprabu0702@gmail.com>
2024-10-17 15:39:39 +0530

    NM Files updated

--- web-technologies-lab-files/Lab-4 22-08-2024/ClassesAndObjects.java
+++ web-technologies-lab-files/Lab-4 22-08-2024/ClassesAndObjects.java
@@ -1,35 +1,69 @@
+
 // import java.lang.*;
+import java.lang.StringBuffer;
 
 // base class
 class Vehicle {
 	private String make;
 	private String model;
 	private int year;
+	private StringBuffer str;
 
-	//constructor
+	// constructor
 	public Vehicle(String make, String model, int year) {
 		this.make = make;
 		this.model = model;
 		this.year = year;
+		this.str = new StringBuffer("");
 	}
 
 	// getters and setters
-	public String getMake() { return this.make; }
-	public String getModel() { return this.model; }
-	public int getYear() { return this.year; }
+	public String getMake() {
+		return this.make;
+	}
 
-	public void setMake(String make) { this.make = make; }
-	public void setModel(String model) { this.model = model; }
-	public void setYear(int year) { this.year = year; }
+	public String getModel() {
+		return this.model;
+	}
+
+	public int getYear() {
+		return this.year;
+	}
+
+	public void setMake(String make) {
+		this.make = make;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
 
 	// display method
 	public void displayInfo() {
-		System.out.println("Make : " + make + "\n" + "Model : " + model + "\n" + "Year : " + year); 
+		System.out.println("Make : " + make + "\n" + "Model : " + model + "\n" + "Year : " + year);
+		str.reverse();
+		System.out.println("Str : " + str);
+		int cap = str.capacity();
+		System.out.println("capacity : " + cap);
+		String str2 = new String(str);
+		System.out.println("Upper case : " + str2.toUpperCase());
+		System.out.println("Lower Case : " + str2.toLowerCase());
+
+		System.out.println("capacity : " + str2.length());
 	}
 
 	// start method overloading
-	public void start() { System.out.println("Vehicle is Starting..."); }
-	public void start(String keyType) { System.out.println("Vehicle is Starting with a " + keyType + " key..."); }
+	public void start() {
+		System.out.println("Vehicle is Starting...");
+	}
+
+	public void start(String keyType) {
+		System.out.println("Vehicle is Starting with a " + keyType + " key...");
+	}
 }
 
 // derived class (inheritance)
@@ -43,8 +77,13 @@ class Car extends Vehicle {
 	}
 
 	// getter and setter for new property
-	public int getNumOfDoors() { return this.numOfDoors; }
-	public void setNumOfDoors(int n) { this.numOfDoors = n; }
+	public int getNumOfDoors() {
+		return this.numOfDoors;
+	}
+
+	public void setNumOfDoors(int n) {
+		this.numOfDoors = n;
+	}
 
 	// override display method
 	@Override
@@ -57,7 +96,7 @@ class Car extends Vehicle {
 // deriver class (motor cycle)
 class MotorCycle extends Vehicle {
 	private boolean hasSideCar;
-	
+
 	// constructor
 	MotorCycle(String make, String model, int year, boolean hasSideCar) {
 		super(make, model, year);
@@ -65,8 +104,13 @@ class MotorCycle extends Vehicle {
 	}
 
 	// getter and setter for new property
-	public boolean getHasSideCar() { return this.hasSideCar; }
-	public void setHasSideCar(boolean h) { this.hasSideCar = h; }
+	public boolean getHasSideCar() {
+		return this.hasSideCar;
+	}
+
+	public void setHasSideCar(boolean h) {
+		this.hasSideCar = h;
+	}
 
 	// override display info
 	@Override
@@ -75,7 +119,6 @@ class MotorCycle extends Vehicle {
 		System.out.println("Has Side Car : " + (hasSideCar ? "YES" : "NO"));
 	}
 }
-
 
 class ClassesAndObjects {
 	public static void main(String args[]) {
@@ -95,9 +138,8 @@ class ClassesAndObjects {
 		car.start();
 
 		System.out.println();
-		
+
 		motorCycle.displayInfo();
 		motorCycle.start();
 	}
 }
-
